1b6bc98fcc8142a7b08bc38db42cea59cce0f6f7
Zach Boulom <zboulom001@student.butte.edu>
2017-08-23 04:00:30 +0000

    lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,6 @@
+Lower number will be called A and Larger number will be called B
+Guess half of B
+If higher guess the number in the middle of the number picked from line 2 and B
+If lower guess the number in the middle of the number picked from line 2 and A
+Each time an incorrect guess is made take away 1 point from 10
+Repeat steps 3 and 4 until correct number is picked
