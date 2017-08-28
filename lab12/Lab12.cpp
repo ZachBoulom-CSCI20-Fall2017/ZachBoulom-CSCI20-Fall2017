1348f11488321051f3d48198083a645d6e9bc948
Zach Boulom <zboulom001@student.butte.edu>
2017-08-28 19:05:21 +0000

    Lab1.2 Commit

---
+++ lab12/Lab12.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "   ___//  "    << endl;
+    cout << "  / .__.\\ "   << endl;
+    cout << "  \\  \\\/ / " << endl;
+    cout << "__/     \\"    << endl;
+    cout << "\\ __     )"   << endl;
+    cout << " \\______/"    << endl;
+    cout << "   |  | "      << endl;
+    cout << "   \"  \""     << endl;
+    
+}
